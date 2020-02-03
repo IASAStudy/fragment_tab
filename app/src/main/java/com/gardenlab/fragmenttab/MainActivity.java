a8c523c6d3263dde240e78b427a03f060ca87bf7
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-03 20:37:12 +0900

    finished

--- app/src/main/java/com/gardenlab/fragmenttab/MainActivity.java
+++ app/src/main/java/com/gardenlab/fragmenttab/MainActivity.java
@@ -1,14 +1,41 @@
 package com.gardenlab.fragmenttab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    FrameLayout container;
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "hello",Toast.LENGTH_SHORT).show();
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container,fragment1).commit();
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment2).commit();
+            }
+        });
+
     }
 }
